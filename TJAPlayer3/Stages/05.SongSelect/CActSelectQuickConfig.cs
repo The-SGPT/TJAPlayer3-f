70f6a685b80cd470937f9406396b06d0cf5e3424
Mr-Ojii <okaschan@gmail.com>
2020-05-07 20:55:50 +0900

    2Pに向け、いろいろと配列化・Dancer追加。え？モーションが少ないって？後で作るから、今はこれで我慢して。

--- TJAPlayer3/Stages/05.SongSelect/CActSelectQuickConfig.cs
+++ TJAPlayer3/Stages/05.SongSelect/CActSelectQuickConfig.cs
@@ -95,7 +95,7 @@ namespace TJAPlayer3
 				" ",
 				new string[] { "OFF", "完走!", "完走!激辛" }) );
 
-			l.Add(new CItemList(nameof(TJAPlayer3.ConfigIni.ShinuchiMode), CItemBase.Eパネル種別.通常, TJAPlayer3.ConfigIni.ShinuchiMode ? 1 : 0, "", "", new string[] { "OFF", "ON" }));
+			l.Add(new CItemList("真打", CItemBase.Eパネル種別.通常, TJAPlayer3.ConfigIni.ShinuchiMode ? 1 : 0, "", "", new string[] { "OFF", "ON" }));
 
 			#endregion
 			#region [ 共通 SET切り替え/More/Return ]
