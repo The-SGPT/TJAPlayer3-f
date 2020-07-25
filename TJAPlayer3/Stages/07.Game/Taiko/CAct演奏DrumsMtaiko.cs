fb39eee078d29f1e87a97438c1dec125d69f0edc
Mr-Ojii <okaschan@gmail.com>
2020-07-25 18:45:09 +0900

    ・難易度選択画面でEnterで演奏オプションを開いた場合、自動的に"ばいそく"が選択されてしまう問題の修正 ・コースシンボルを1Pと2Pで分離

--- TJAPlayer3/Stages/07.Game/Taiko/CAct演奏DrumsMtaiko.cs
+++ TJAPlayer3/Stages/07.Game/Taiko/CAct演奏DrumsMtaiko.cs
@@ -296,9 +296,9 @@ namespace TJAPlayer3
 				//    else if( CDTXMania.ConfigIni.eSTEALTH == Eステルスモード.DORON )
 				//        this.txオプションパネル_特殊.t2D描画( CDTXMania.app.Device, 0, 300, new Rectangle( 0, 44, 162, 44 ) );
 				//}
-				if (TJAPlayer3.Tx.Couse_Symbol[TJAPlayer3.stage選曲.n確定された曲の難易度[0]] != null)
+				if (TJAPlayer3.Tx.Couse_Symbol[TJAPlayer3.stage選曲.n確定された曲の難易度[i]] != null)
 				{
-					TJAPlayer3.Tx.Couse_Symbol[TJAPlayer3.stage選曲.n確定された曲の難易度[0]].t2D描画(TJAPlayer3.app.Device,
+					TJAPlayer3.Tx.Couse_Symbol[TJAPlayer3.stage選曲.n確定された曲の難易度[i]].t2D描画(TJAPlayer3.app.Device,
 						TJAPlayer3.Skin.Game_CourseSymbol_X[i],
 						TJAPlayer3.Skin.Game_CourseSymbol_Y[i]
 						);
@@ -315,9 +315,8 @@ namespace TJAPlayer3
 					}
 
 				}
-
-
 			}
+
 			if (TJAPlayer3.Tx.Taiko_NamePlate[0] != null)
 			{
 				TJAPlayer3.Tx.Taiko_NamePlate[0].t2D描画(TJAPlayer3.app.Device, TJAPlayer3.Skin.Game_Taiko_NamePlate_X[0], TJAPlayer3.Skin.Game_Taiko_NamePlate_Y[0]);
