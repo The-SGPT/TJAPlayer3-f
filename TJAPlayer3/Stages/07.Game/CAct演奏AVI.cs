e16bdb2fa1435937be1ebcc2ac118958aecc163b
Mr-Ojii <okaschan@gmail.com>
2020-09-06 23:46:44 +0900

    整理(3)

--- TJAPlayer3/Stages/07.Game/CAct演奏AVI.cs
+++ TJAPlayer3/Stages/07.Game/CAct演奏AVI.cs
@@ -35,8 +35,6 @@ namespace TJAPlayer3
 				{
 					this.framewidth = (uint)this.dsBGV.dshow.n幅px;
 					this.frameheight = (uint)this.dsBGV.dshow.n高さpx;
-					float f拡大率x;
-					float f拡大率y;
 					this.fAVIアスペクト比 = ((float)this.framewidth) / ((float)this.frameheight);
 
 					if ( fAVIアスペクト比 < 1.77f )
@@ -48,7 +46,6 @@ namespace TJAPlayer3
 						this.n移動開始時刻ms = (n移動開始時刻ms != -1) ? n移動開始時刻ms : (long)(CSound管理.rc演奏用タイマ.n現在時刻 * (((double)TJAPlayer3.ConfigIni.n演奏速度) / 20.0));
 						this.n前回表示したフレーム番号 = -1;
 
-						this.vclip = new Vector3(1.42f, 1.42f, 1f);
 						this.dsBGV = null;
 						#endregion
 					}
@@ -62,26 +59,6 @@ namespace TJAPlayer3
 					{
 						//旧企画クリップだった場合
 						this.ratio1 = 720f / ((float)this.frameheight);
-						this.position = (int)((1280f - (this.framewidth * this.ratio1)) / 2f);
-						int num = (int)(this.framewidth * this.ratio1);
-						if (num <= 565)
-						{
-							this.position = 295 + ((int)((565f - (this.framewidth * this.ratio1)) / 2f));
-							this.i1 = 0;
-							this.i2 = (int)this.framewidth;
-							this.rec = new Rectangle(0, 0, 0, 0);
-							this.rec3 = new Rectangle(0, 0, 0, 0);
-							this.rec2 = new Rectangle(0, 0, (int)this.framewidth, (int)this.frameheight);
-						}
-						else
-						{
-							this.position = 295 - ((int)(((this.framewidth * this.ratio1) - 565f) / 2f));
-							this.i1 = (int)(((float)(295 - this.position)) / this.ratio1);
-							this.i2 = (int)((565f / ((float)num)) * this.framewidth);
-							this.rec = new Rectangle(0, 0, this.i1, (int)this.frameheight);
-							this.rec3 = new Rectangle(this.i1 + this.i2, 0, (((int)this.framewidth) - this.i1) - this.i2, (int)this.frameheight);
-							this.rec2 = new Rectangle(this.i1, 0, this.i2, (int)this.frameheight);
-						}
 						this.tx描画用.vc拡大縮小倍率.X = this.ratio1;
 						this.tx描画用.vc拡大縮小倍率.Y = this.ratio1;
 					}
@@ -89,43 +66,10 @@ namespace TJAPlayer3
 					{
 						//ワイドクリップの処理
 						this.ratio1 = 1280f / ((float)this.framewidth);
-						this.position = (int)((720f - (this.frameheight * this.ratio1)) / 2f);
-						this.i1 = (int)(this.framewidth * 0.23046875);
-						this.i2 = (int)(this.framewidth * 0.44140625);
-						this.rec = new Rectangle(0, 0, this.i1, (int)this.frameheight);
-						this.rec2 = new Rectangle(this.i1, 0, this.i2, (int)this.frameheight);
-						this.rec3 = new Rectangle(this.i1 + this.i2, 0, (((int)this.framewidth) - this.i1) - this.i2, (int)this.frameheight);
 						this.tx描画用.vc拡大縮小倍率.X = this.ratio1;
 						this.tx描画用.vc拡大縮小倍率.Y = this.ratio1;
 					}
 
-
-					if (this.framewidth > 420)
-					{
-						f拡大率x = 420f / ((float)this.framewidth);
-					}
-					else
-					{
-						f拡大率x = 1f;
-					}
-					if (this.frameheight > 580)
-					{
-						f拡大率y = 580f / ((float)this.frameheight);
-					}
-					else
-					{
-						f拡大率y = 1f;
-					}
-					if (f拡大率x > f拡大率y)
-					{
-						f拡大率x = f拡大率y;
-					}
-					else
-					{
-						f拡大率y = f拡大率x;
-					}
-
-					this.smallvc = new Vector3(f拡大率x, f拡大率y, 1f);
 					#endregion
 				}
 
@@ -170,13 +114,6 @@ namespace TJAPlayer3
 				if( this.dsBGV.dshow != null )
 					this.dsBGV.dshow.MediaCtrl.Stop();
 				this.bDShowクリップを再生している = false;
-			}
-		}
-		public void Cont( int n再開時刻ms )
-		{
-			if ( ( this.rAVI != null ) && ( this.rAVI.avi != null ) )
-			{
-				this.n移動開始時刻ms = n再開時刻ms;
 			}
 		}
 		public unsafe int t進行描画( int x, int y )
@@ -224,11 +161,10 @@ namespace TJAPlayer3
 				//2014.11.17 kairera0467 AVIが無い状態でAVIのフレームカウントをさせるとエラーを吐くため、かなり雑ではあるが対策。
 				if( ( this.n総移動時間ms == 0 ) && this.rAVI.avi != null ? ( frameNoFromTime >= this.rAVI.avi.GetMaxFrameCount() ) : false )
 				{
-					this.n移動開始時刻ms = -1L;
+					this.n移動開始時刻ms = -1;
 				}
 				if((((this.n前回表示したフレーム番号 != frameNoFromTime) || !this.bフレームを作成した)) && (fAVIアスペクト比 < 1.77f ))
 				{
-					this.pBmp = this.rAVI.avi.GetFramePtr(frameNoFromTime);
 					this.n前回表示したフレーム番号 = frameNoFromTime;
 					this.bフレームを作成した = true;
 				}
@@ -273,12 +209,7 @@ namespace TJAPlayer3
 					num4 = (long)(CSound管理.rc演奏用タイマ.n現在時刻 * (((double)TJAPlayer3.ConfigIni.n演奏速度) / 20.0));
 				}
 				time = (int)(((CSound管理.rc演奏用タイマ.n現在時刻 * (((double)TJAPlayer3.ConfigIni.n演奏速度) / 20.0)) - num4));
-				if ( num3 == 0 )
-				{
-					rectangle = new Rectangle(0, 0, size3.Width, size3.Height);
-					rectangle2 = new Rectangle(0, 0, size3.Width, size3.Height);
-				}
-				else
+				if ( num3 != 0 )
 				{
 					double num5 = ( (double) time ) / ( (double) num3 );
 					Size size5 = new Size( size3.Width + ( (int) ( ( - size3.Width ) * num5 ) ), size3.Height + ( (int) ( ( - size3.Height ) * num5 ) ) );
@@ -292,22 +223,6 @@ namespace TJAPlayer3
 					{
 						return 0;
 					}
-					if ( rectangle.X < 0 )
-					{
-						int num6 = -rectangle.X;
-						rectangle2.X += num6;
-						rectangle2.Width -= num6;
-						rectangle.X = 0;
-						rectangle.Width -= num6;
-					}
-					if ( rectangle.Y < 0 )
-					{
-						int num7 = -rectangle.Y;
-						rectangle2.Y += num7;
-						rectangle2.Height -= num7;
-						rectangle.Y = 0;
-						rectangle.Height -= num7;
-					}
 					if ( rectangle.Right > szフレーム幅.Width )
 					{
 						int num8 = rectangle.Right - szフレーム幅.Width;
@@ -319,22 +234,6 @@ namespace TJAPlayer3
 						int num9 = rectangle.Bottom - szフレーム幅.Height;
 						rectangle2.Height -= num9;
 						rectangle.Height -= num9;
-					}
-					if ( rectangle2.X < 0 )
-					{
-						int num10 = -rectangle2.X;
-						rectangle.X += num10;
-						rectangle.Width -= num10;
-						rectangle2.X = 0;
-						rectangle2.Width -= num10;
-					}
-					if ( rectangle2.Y < 0 )
-					{
-						int num11 = -rectangle2.Y;
-						rectangle.Y += num11;
-						rectangle.Height -= num11;
-						rectangle2.Y = 0;
-						rectangle2.Height -= num11;
 					}
 					if ( rectangle2.Right > sz最大フレーム幅.Width )
 					{
@@ -382,36 +281,6 @@ namespace TJAPlayer3
 						}
 						#endregion
 					}
-				}
-				else if ( ( this.tx描画用 != null ) && ( this.n総移動時間ms != -1 ) )
-				{
-					if ( this.bフレームを作成した && ( this.pBmp != IntPtr.Zero ) )
-					{
-						DataRectangle rectangle3 = this.tx描画用.texture.LockRectangle( 0, LockFlags.None );
-						DataStream data = new DataStream(rectangle3.DataPointer, this.tx描画用.szテクスチャサイズ.Width * rectangle3.Pitch, true, false);
-						int num14 = rectangle3.Pitch / this.tx描画用.szテクスチャサイズ.Width;
-						BitmapUtil.BITMAPINFOHEADER* pBITMAPINFOHEADER = (BitmapUtil.BITMAPINFOHEADER*) this.pBmp.ToPointer();
-						if ( pBITMAPINFOHEADER->biBitCount == 0x18 )
-						{
-							switch ( num14 )
-							{
-								case 2:
-									this.rAVI.avi.tBitmap24ToGraphicsStreamR5G6B5( pBITMAPINFOHEADER, data, this.tx描画用.szテクスチャサイズ.Width, this.tx描画用.szテクスチャサイズ.Height );
-									break;
-
-								case 4:
-									this.rAVI.avi.tBitmap24ToGraphicsStreamX8R8G8B8( pBITMAPINFOHEADER, data, this.tx描画用.szテクスチャサイズ.Width, this.tx描画用.szテクスチャサイズ.Height );
-									break;
-							}
-						}
-						this.tx描画用.texture.UnlockRectangle( 0 );
-						this.bフレームを作成した = false;
-					}
-					double dbAVI比率 = this.rAVI.avi.nフレーム幅 / this.rAVI.avi.nフレーム高さ;
-
-					//とりあえず16:9以外は再生しない。
-					if( dbAVI比率 < 1.77 )
-						this.tx描画用.t2D描画( TJAPlayer3.app.Device, 0, 0 );
 				}
 			}
 			return 0;
@@ -493,7 +362,6 @@ namespace TJAPlayer3
 				this.tx描画用 = new CTexture( TJAPlayer3.app.Device, 1280, 720, TJAPlayer3.app.GraphicsDeviceManager.CurrentSettings.BackBufferFormat, Pool.Managed );
 				this.tx窓描画用 = new CTexture( TJAPlayer3.app.Device, 1280, 720, TJAPlayer3.app.GraphicsDeviceManager.CurrentSettings.BackBufferFormat, Pool.Managed );
 			}
-			
 		}
 
 		// CActivity 実装
@@ -504,7 +372,6 @@ namespace TJAPlayer3
 			this.n移動開始時刻ms = -1;
 			this.n前回表示したフレーム番号 = -1;
 			this.bフレームを作成した = false;
-			this.pBmp = IntPtr.Zero;
 			base.On活性化();
 		}
 		public override void OnManagedリソースの作成()
@@ -554,20 +421,8 @@ namespace TJAPlayer3
 		public float fAVIアスペクト比;
 		private uint frameheight;
 		private uint framewidth;
-		private int i1;
-		private int i2;
-		private int position;
-		private int position2;
 		private float ratio1;
-		private float ratio2;
-		private Rectangle rec;
-		private Rectangle rec2;
-		private Rectangle rec3;
-		public Vector3 smallvc;
-		private Vector3 vclip;
-		public Vector3 vector;
 
-		private IntPtr pBmp;
 		private CDTX.CAVI rAVI;
 		private CTexture tx描画用;
 		private CTexture tx窓描画用;
