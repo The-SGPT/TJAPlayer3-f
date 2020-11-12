3b73f5d8974565d6dbf65b6ffff9a17d47996afc
Mr-Ojii <okaschan@gmail.com>
2020-11-12 23:33:17 +0900

    Syste.Windows.Formsを削減

--- FDK19/コード/01.フレームワーク/Core/Game.cs
+++ FDK19/コード/01.フレームワーク/Core/Game.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Threading;
-using System.Windows.Forms;
 using System.Reflection;
 using System.IO;
 using System.Text;
--- FDK19/コード/02.入力/CInputKeyboard.cs
+++ FDK19/コード/02.入力/CInputKeyboard.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Diagnostics;
-using System.Windows.Forms;
 using OpenTK.Input;
 
 using SlimDXKey = SlimDXKeys.Key;
